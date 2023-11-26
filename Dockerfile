c436adf5c5b840da39102d5e806da2b4a4ae4c4d
nils.lueem <nils.lueem@inpeek.ch>
2023-11-26 15:45:05 +0100

    new dockerfile

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+./mvnw package
+docker build -f src/main/docker/Dockerfile.jvm -t quarkus/code-with-quarkus-jvm .
+docker run -i --rm -p 8080:8080 quarkus/code-with-quarkus-jvm
--- src/main/java/dev/lueem/ai/OpenAiClient.java
+++ src/main/java/dev/lueem/ai/OpenAiClient.java
@@ -1,52 +1,35 @@
 package dev.lueem.ai;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.Properties;
+import java.io.FileInputStream;
+import java.io.IOException;
 
-import com.theokanning.openai.completion.CompletionRequest;
 import com.theokanning.openai.completion.chat.ChatCompletionRequest;
-import com.theokanning.openai.completion.chat.ChatCompletionRequest.ChatCompletionRequestFunctionCall;
-import com.theokanning.openai.completion.chat.ChatFunction;
 import com.theokanning.openai.completion.chat.ChatMessage;
 import com.theokanning.openai.completion.chat.ChatMessageRole;
-import com.theokanning.openai.service.FunctionExecutor;
 import com.theokanning.openai.service.OpenAiService;
-
-import dev.lueem.ai.OpenAiFunctions.Articles;
-import dev.lueem.ai.OpenAiFunctions.ArticlesResponse;
 
 public class OpenAiClient {
 
     private OpenAiService service;
-    private FunctionExecutor functionExecutor;
-    private String documentContent;
+    private Properties properties;
 
     public OpenAiClient() {
+        properties = new Properties();
+        try {
+            properties.load(new FileInputStream("config.properties"));
 
-        service = new OpenAiService("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");
+            String apiKey = properties.getProperty("openai.api.key");
+            service = new OpenAiService(apiKey);
 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public String askQuestion(String question) {
-
-        // Verwenden Sie ein CompletionRequest-Objekt statt ChatCompletionRequest
-        // (sofern Ihre Bibliothek dies unterstützt)
-        // CompletionRequest request = CompletionRequest.builder()
-        // .model("davinci-002")
-        // .prompt(question) // Verwenden Sie `prompt` anstelle von `messages`
-        // .n(1)
-        // .maxTokens(1000)
-        // .build();
-
-        // // Die Antwort-Methode kann sich je nach Ihrer Client-Bibliothek ändern
-        // String response =
-        // service.createCompletion(request).getChoices().get(0).getText(); // Annahme,
-        // dass es eine
-        // // `getText` Methode gibt
-
-        // return response;
-        // Erstellen Sie eine einfache Nachrichtenliste, die nur Ihre Frage enthält
         List<ChatMessage> messages = new ArrayList<>();
         messages.add(new ChatMessage(ChatMessageRole.USER.value(), question));
 
@@ -60,5 +43,4 @@ public class OpenAiClient {
         ChatMessage responseMessage = service.createChatCompletion(request).getChoices().get(0).getMessage();
         return responseMessage.getContent();
     }
-
 }
